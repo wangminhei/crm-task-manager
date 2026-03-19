cbea5c2e6d7b8d23b2cb45bd1e429290fae109f7
wangminhei <41012563+wangminhei@users.noreply.github.com>
2026-03-19 21:09:03 +0700

    Add seed data for users, customers, and tasks

---
+++ database/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name) VALUES ('Admin');
+INSERT INTO customers (name) VALUES ('Khách A');
+
+INSERT INTO tasks (title, customer_id, assigned_to)
+VALUES ('Task test đầu tiên', 1, 1);
